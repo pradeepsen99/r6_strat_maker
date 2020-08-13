fc8ffa64e2de9cc706553b9a934037c3c657e7b1
Pradeep Senthil <pradeepsen99@gmail.com>
2020-08-13 18:25:17 -0500

    Updates

---
+++ precache-manifest.e9269740feb06c3b460476195f8211f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125a80c33f63bfb7c633805c40cf0abc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "522697528c814bc9f17d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a6eef038db9ed649a93",
+    "url": "/static/js/2.aabf27c1.chunk.js"
+  },
+  {
+    "revision": "74549c08325e899dd17d787a1ff374cf",
+    "url": "/static/js/2.aabf27c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "522697528c814bc9f17d",
+    "url": "/static/js/main.b4c35a96.chunk.js"
+  },
+  {
+    "revision": "2421ba3f3d541ab88ebd",
+    "url": "/static/js/runtime-main.de01944b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
